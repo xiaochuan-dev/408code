c78da2be53db13a5303082ce2b4ade4fd133e4ec
freexiaochuan <w2239559319@myyahoo.com>
2026-02-06 08:02:33 +0000

    u

--- operationSystem/src/sync.h
+++ operationSystem/src/sync.h
@@ -4,12 +4,16 @@
 #include <chrono>
 #include <print>
 
+/**
+ * 注意这里的线程会退出，在题目中一般是不会退出一直循环代码的一个服务（代码最外层有一个一直运行的while）
+ */
+
 namespace operationSystem
 {
-    /**
-     * 但标志法
-     * 违反空闲让进
-     */
+/**
+ * 但标志法
+ * 违反空闲让进
+*/
 void singleFlag()
 {
     // 只适合交替进入
@@ -49,4 +53,68 @@ void singleFlagFail()
         std::println("t2进入剩余区");
     });
 }
+
+/**
+ * 双标志先检查法
+ */
+void twoFlagsPreCheck()
+{
+    static bool flags[] = { false, false };
+
+    std::jthread t1([]() {
+        /**
+         * 如果对让正在使用就等待
+         */
+        while (flags[1]);
+
+        flags[0] = true;
+        std::println("t1进入临界区");
+        flags[0] = false;
+
+        std::println("t1进入剩余区");
+    });
+    std::jthread t2([]() {
+        while (flags[0]);
+
+        flags[1] = true;
+        std::println("t2进入临界区");
+        flags[1] = false;
+        std::println("t2进入剩余区");
+    });
+}
+
+/**
+ * 双标志先检查法
+ * 违反了忙则等待，下面的代码两个线程可以一起访问临界区
+ */
+void twoFlagsPreCheckFail()
+{
+    static bool flags[] = { false, false };
+
+    std::jthread t1([]() {
+        /**
+         * 如果对方正在使用就等待
+         */
+        while (flags[1]);
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::println("t1被挂起2秒");
+
+        flags[0] = true;
+        std::println("t1进入临界区");
+        flags[0] = false;
+
+        std::println("t1进入剩余区");
+    });
+    std::jthread t2([]() {
+        while (flags[0]);
+
+        flags[1] = true;
+        std::println("t2进入临界区");
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        flags[1] = false;
+        std::println("t2进入剩余区");
+    });
+}
+
 }
--- operationSystem/test/sync_test.cpp
+++ operationSystem/test/sync_test.cpp
@@ -3,6 +3,29 @@
 
 TEST(SYNCTEST, singleFlagTest) 
 {
+    std::println("start singleFlagTest");
     operationSystem::singleFlag();
+    std::println("end singleFlagTest");
+}
+
+TEST(SYNCTEST, singleFlagTestFail) 
+{
+    std::println("start singleFlagTestFail");
     operationSystem::singleFlagFail();
+    std::println("end singleFlagTestFail");
+}
+
+
+TEST(SYNCTEST, twoFlagsPreCheckTest) 
+{
+    std::println("start twoFlagsPreCheckTest");
+    operationSystem::twoFlagsPreCheck();
+    std::println("end twoFlagsPreCheckTest");
+}
+
+TEST(SYNCTEST, twoFlagsPreCheckTestFail) 
+{
+    std::println("start twoFlagsPreCheckTestFail");
+    operationSystem::twoFlagsPreCheckFail();
+    std::println("end twoFlagsPreCheckTestFail");
 }
